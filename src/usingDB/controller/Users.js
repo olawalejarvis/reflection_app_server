4527865815ddb5e9d5cc94aa4f7f4c85b188b0d2
sholabioku <bilushonline@gmail.com>
2020-04-23 18:08:19 +0100

    Add getAll method for users

--- src/usingDB/controller/Users.js
+++ src/usingDB/controller/Users.js
@@ -6,16 +6,16 @@ import Helper from './Helper';
 const User = {
   /**
    * Create A User
-   * @param {object} req 
+   * @param {object} req
    * @param {object} res
-   * @returns {object} reflection object 
+   * @returns {object} reflection object
    */
   async create(req, res) {
     if (!req.body.email || !req.body.password) {
-      return res.status(400).send({'message': 'Some values are missing'});
+      return res.status(400).send({ message: 'Some values are missing' });
     }
     if (!Helper.isValidEmail(req.body.email)) {
-      return res.status(400).send({ 'message': 'Please enter a valid email address' });
+      return res.status(400).send({ message: 'Please enter a valid email address' });
     }
     const hashPassword = Helper.hashPassword(req.body.password);
 
@@ -23,71 +23,87 @@ const User = {
       users(id, email, password, created_date, modified_date)
       VALUES($1, $2, $3, $4, $5)
       returning *`;
-    const values = [
-      uuidv4(),
-      req.body.email,
-      hashPassword,
-      moment(new Date()),
-      moment(new Date())
-    ];
+    const values = [uuidv4(), req.body.email, hashPassword, moment(new Date()), moment(new Date())];
 
     try {
       const { rows } = await db.query(createQuery, values);
       const token = Helper.generateToken(rows[0].id);
       return res.status(201).send({ token });
-    } catch(error) {
+    } catch (error) {
       if (error.routine === '_bt_check_unique') {
-        return res.status(400).send({ 'message': 'User with that EMAIL already exist' })
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
       }
       return res.status(400).send(error);
     }
   },
   /**
    * Login
-   * @param {object} req 
+   * @param {object} req
    * @param {object} res
-   * @returns {object} user object 
+   * @returns {object} user object
    */
   async login(req, res) {
     if (!req.body.email || !req.body.password) {
-      return res.status(400).send({'message': 'Some values are missing'});
+      return res.status(400).send({ message: 'Some values are missing' });
     }
     if (!Helper.isValidEmail(req.body.email)) {
-      return res.status(400).send({ 'message': 'Please enter a valid email address' });
+      return res.status(400).send({ message: 'Please enter a valid email address' });
     }
     const text = 'SELECT * FROM users WHERE email = $1';
     try {
       const { rows } = await db.query(text, [req.body.email]);
       if (!rows[0]) {
-        return res.status(400).send({'message': 'The credentials you provided is incorrect'});
+        return res.status(400).send({ message: 'The credentials you provided is incorrect' });
       }
-      if(!Helper.comparePassword(rows[0].password, req.body.password)) {
-        return res.status(400).send({ 'message': 'The credentials you provided is incorrect' });
+      if (!Helper.comparePassword(rows[0].password, req.body.password)) {
+        return res.status(400).send({ message: 'The credentials you provided is incorrect' });
       }
       const token = Helper.generateToken(rows[0].id);
       return res.status(200).send({ token });
-    } catch(error) {
-      return res.status(400).send(error)
+    } catch (error) {
+      return res.status(400).send(error);
     }
   },
+
+  /**
+   * Get All Users
+   * @param {object} req
+   * @param {object} res
+   * @returns {obkect} Users array
+   */
+
+  async getAll(req, res) {
+    const findAllQuery = 'SELECT * FROM users';
+    try {
+      const { rows, rowCount } = await db.query(findAllQuery);
+      return res.status(200).send({
+        message: 'All users were fetched',
+        rowCount,
+        rows,
+      });
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+
   /**
    * Delete A User
-   * @param {object} req 
-   * @param {object} res 
-   * @returns {void} return status code 204 
+   * @param {object} req
+   * @param {object} res
+   * @returns {void} return status code 204
    */
   async delete(req, res) {
     const deleteQuery = 'DELETE FROM users WHERE id=$1 returning *';
     try {
       const { rows } = await db.query(deleteQuery, [req.user.id]);
-      if(!rows[0]) {
-        return res.status(404).send({'message': 'user not found'});
+      if (!rows[0]) {
+        return res.status(404).send({ message: 'user not found' });
       }
-      return res.status(204).send({ 'message': 'deleted' });
-    } catch(error) {
+      return res.status(204).send({ message: 'deleted' });
+    } catch (error) {
       return res.status(400).send(error);
     }
-  }
-}
+  },
+};
 
 export default User;
